7d3fc46f69bc9ca4f372490ade66b6fc7cd33d13
RedAces <redaces@gmx.de>
2025-03-06 07:36:03 +0100

    add addon category hide debug message

---
+++ scripts/stylua.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+
+D:\Database\Downloads\wow\stylua\stylua.exe .
---
+++ scripts/to-wow.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+
+xcopy "Core.lua" "f:\Games\World of Warcraft\_retail_\Interface\AddOns\CDButtonGlow\Core.lua" /s /y
+xcopy "Options.lua" "f:\Games\World of Warcraft\_retail_\Interface\AddOns\CDButtonGlow\Options.lua" /s /y
